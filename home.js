fcf92d7373f147fce9d56a10fbfc060a04639152
slackyboating <slackyboating@Demos-MacBook-Pro.local>
2022-03-02 01:47:02 +0800

    first commit

---
+++ home.js
@@ -0,0 +1,35 @@
+const axios = require('axios');
+const _ = require('lodash');
+const core = require('@actions/core');
+const message = require('./message');
+
+const REQUIRED_ENV_VARS = [
+  "GITHUB_EVENT_PATH",
+  "SLACK_WEBHOOK",
+];
+
+try {
+  _.forEach(REQUIRED_ENV_VARS, env => {
+    if (_.isEmpty(process.env[env])) {
+      process.exitCode = 1;
+      throw new Error(`Missing environment variable. ${env} is required.`);
+    }
+  });
+} catch (e) { core.setFailed(e.message); }
+
+
+if (!process.exitCode) {
+  core.info("Sending message ...");
+
+  axios
+    .post(process.env.SLACK_WEBHOOK, message.get())
+    .then(() => {
+      process.exitCode = 0;
+      return core.info('Message sent! Shutting down ...');
+    })
+    .catch((err) => {
+      process.exitCode = 1;
+      const errMessage = err.response ? err.response.data : err.message;
+      return core.setFailed(`Error: ${errMessage}`);
+    });
+}
